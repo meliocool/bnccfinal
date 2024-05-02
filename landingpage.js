edd547bf5a7231b5e033e6f2462422077e3866aa
melio <dhitanimam05@gmail.com>
2024-05-03 00:35:31 +0700

    dikitlagiselesai

--- landingpage.js
+++ landingpage.js
@@ -1,10 +1,25 @@
-const getItems = document.querySelectorAll(".left-side ul li a");
+// const getItems = document.querySelectorAll(".left-side ul li a");
 
-getItems.forEach(item => {
+// getItems.forEach(item => {
+//     item.addEventListener("click", function(e){
+//         for(var i = 0; i < getItems.length; i++){
+//             getItems[i].classList.remove("active");
+//         }
+//         this.classList.add("active");
+//     });
+// });
+
+const getItems = document.querySelectorAll(".left-side ul li a");
+const explanations = document.querySelectorAll(".right-side ul li .explanation");
+
+getItems.forEach((item, index) => {
     item.addEventListener("click", function(e){
-        for(var i = 0; i < getItems.length; i++){
-            getItems[i].classList.remove("active");
-        }
+        // Remove active class from all bullet points and explanations
+        getItems.forEach(item => item.classList.remove("active"));
+        explanations.forEach(explanation => explanation.classList.remove("active"));
+        
+        // Add active class to the clicked bullet point and corresponding explanation
         this.classList.add("active");
+        explanations[index].classList.add("active");
     });
 });
